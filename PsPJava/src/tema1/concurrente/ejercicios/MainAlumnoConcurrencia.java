68f55ff700e271f592590ead154c0cc852b361b4
amna <antoniomiguel2001@hotmail.es>
2022-11-01 13:54:02 +0100

    fin ej 1.20

--- PsPJava/src/tema1/concurrente/ejercicios/MainAlumnoConcurrencia.java
+++ PsPJava/src/tema1/concurrente/ejercicios/MainAlumnoConcurrencia.java
@@ -24,12 +24,12 @@ public class MainAlumnoConcurrencia {
 		Runnable numDieciNueve = () ->{
 			alumno.alumDieciNueveAnios();
 		};
-		long init = System.nanoTime();
-		//Instanciación de las interfaces a los hilos
+		
 		Thread hiloUno = new Thread(media);
 		Thread hiloDos = new Thread(mayoresVeCinco);
 		Thread hiloTres = new Thread(entreEdad);
 		Thread hiloCuatro = new Thread(numDieciNueve);
+		long init = System.nanoTime();
 		//Empiezan la ejecución de los hilos
 		hiloUno.start();
 		hiloDos.start();
@@ -37,7 +37,7 @@ public class MainAlumnoConcurrencia {
 		hiloCuatro.start();
 		long fin = System.nanoTime();
 		long tiempo = fin - init;
-		//0 ms(180000~ ns). Lo pongo en nanosegundos porque sino siempre saldría 0 ms
+		//0 ms(150000~ ns). Lo pongo en nanosegundos porque sino siempre saldría 0 ms
       System.out.println(tiempo +" ns");
 	}
 
