3a58a25c6055216babbb1d3a89f2e8435c3dad5e
amna <antoniomiguel2001@hotmail.es>
2022-11-24 13:39:40 +0100

    fino ej 2_2

--- PsPJava/src/tema2/hilos/DadosTirada.java
+++ PsPJava/src/tema2/hilos/DadosTirada.java
@@ -1,33 +1,60 @@
 package tema2.hilos;
 
 import java.util.ArrayList;
-
+/**
+ * Clase que crea hilos a partir de la interfaz Runnable
+ * utilizando expresiones lambda para crear un juego
+ * de tiradas de dados
+ * @author amna
+ * @version 1.0
+ */
 public class DadosTirada {
 	private ArrayList<Integer> tiradaNum = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 		DadosTirada dadosTirada = new DadosTirada();
+		ThreadGroup tg = new ThreadGroup("tiradas");
+		//implementación interfaz a partir de expresines lambda
 		Runnable tirada = () -> {
-			int suertes = (int) (Math.random()*6+1);
+			int suertes = (int) (Math.random() * 6 + 1);
+			//se añade a un ArrayList el resultado aleatorio entre 1 y 6
 			dadosTirada.tiradaNum.add(suertes);
-			System.out.println("Jugador: "+Thread.currentThread().getName()+" Tirada: "+suertes);
+			System.out.println(Thread.currentThread().getName() + " Tirada: " + suertes);
 		};
-		
 		for (int i = 0; i < 3; i++) {
-			Thread h = new Thread(tirada);
+			//instanciación de un objeto Thread y lo añade al ThreadGroup creado
+			Thread h = new Thread(tg,tirada,"Jugador "+i);
+			//inicio del hilo
 			h.start();
 			try {
+				//pone en espera al hilo hasta que muera
 				h.join();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-
+		System.out.println("---------------------"+"\n"+"El juego ha terminado");
 		
-		
+		System.out.println("----------------------------");
+		if (dadosTirada.tiradaNum.get(0) > dadosTirada.tiradaNum.get(1)
+				&& dadosTirada.tiradaNum.get(0) > dadosTirada.tiradaNum.get(2)) {
+			System.out.println("Ganó el Jugador 0");
+		} else if (dadosTirada.tiradaNum.get(1) > dadosTirada.tiradaNum.get(0)
+				&& dadosTirada.tiradaNum.get(1) > dadosTirada.tiradaNum.get(2)) {
+			System.out.println("Ganó el Jugador 1");
+		} else if (dadosTirada.tiradaNum.get(2) > dadosTirada.tiradaNum.get(0)
+				&& dadosTirada.tiradaNum.get(2) > dadosTirada.tiradaNum.get(1)) {
+			System.out.println("Ganó el Jugador 2");
+		} else {
+			System.out.println("Empate");
+		}
+		System.out.println("----------------------------");
+		System.out.println("Suma resultados: "
+				+ (dadosTirada.tiradaNum.get(0) + dadosTirada.tiradaNum.get(1) + dadosTirada.tiradaNum.get(2)));
 	}
 
-	public DadosTirada() {}
+	public DadosTirada() {
+	}
 
 	public DadosTirada(ArrayList<Integer> tiradaNum) {
 		this.tiradaNum = tiradaNum;
@@ -40,6 +67,5 @@ public class DadosTirada {
 	public void setTiradaNum(ArrayList<Integer> tiradaNum) {
 		this.tiradaNum = tiradaNum;
 	}
-	
 
 }
